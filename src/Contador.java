9fe6161d52893b447ecda6633822292cb7b2d21a
jessedelima <jessedelima@hotmail.com>
2023-08-24 21:36:13 -0300

    Desafio controle Fluxo
    Signed-off-by: jessedelima <jessedelima@hotmail.com>

---
+++ src/Contador.java
@@ -0,0 +1,35 @@
+package src;
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        int parametroUm = 12;
+        int parametroDois = 30;
+
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            // Imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        // Realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
